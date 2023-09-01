623edfe670e1ecf2c8218708d5da49840e3a8e46
Nkaleth <nseguralu@gmail.com>
2023-09-01 12:32:20 -0500

    ADD chat component and styles using SASS syntax

---
+++ src/components/Chat.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Avatar, IconButton } from '@mui/material';
+import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material';
+import '../styles/Chat.scss';
+
+function Chat() {
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body"></div>
+      <div className="chat__footer"></div>
+    </div>
+  );
+}
+
+export default Chat;
